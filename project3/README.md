de17a879ad1549741deacb405388cf490b075453
sgallon <shcmsgallon@outlook.com>
2021-06-02 16:31:46 +0800

    add project 3

---
+++ project3/README.md
@@ -0,0 +1,1 @@
+# Project 3 - Scene Text Recognition
---
+++ project3/code/config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time        : 2021/5/27 11:15
+# @Author      : sgallon
+# @Email       : shcmsgallon@outlook.com
+# @File        : config.py
+# @Description :
+
+import os
+
+# HOME = r"/home/jupyter/sjl/neural-network-deep-learning/project3"
+HOME = r"/Users/sgallon/Documents/Docus/kjzl/5B/神经网络与深度学习 Neural Network and Deep " \
+       r"Learning/neural-network-deep-learning/project3"
+
+DATA_ROOT = os.path.join(HOME, "data")
