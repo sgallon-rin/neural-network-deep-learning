319ab1ed82bd295ec061d757b37f39d90944dbd1
sgallon <shcmsgallon@outlook.com>
2021-06-07 14:32:56 +0800

    change hyperparams for training EAST; add log and start_epoch

--- project3/code/train_EAST.py
+++ project3/code/train_EAST.py
@@ -20,9 +20,10 @@ from config import HOME, DATA_ROOT
 from logger_utils import logger
 
 
-def train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers, epoch_iter, interval):
-    file_num = len(os.listdir(train_img_path))
+def train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers, epoch_iter, interval,
+          start_epoch=0, start_epoch_pth_path=None):
     trainset = custom_dataset(train_img_path, train_gt_path)
+    file_num = len(trainset)
     train_loader = data.DataLoader(trainset, batch_size=batch_size, \
                                    shuffle=True, num_workers=num_workers, drop_last=True)
 
@@ -30,16 +31,21 @@ def train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers,
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     logger.info("Use device: {}".format(device))
     model = EAST(pretrained=False)
+    logger.info("Start from epoch {}".format(start_epoch))
+    if start_epoch > 0:
+        model.load_state_dict(torch.load(start_epoch_pth_path))
+        logger.info("Successfully loaded start state dict from {}".format(start_epoch_pth_path))
     data_parallel = False
     if torch.cuda.device_count() > 1:
         model = nn.DataParallel(model)
         data_parallel = True
+    logger.info("Use data parallel" if data_parallel else "Do not use data parallel")
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[epoch_iter // 2], gamma=0.1)
 
     logger.info("Training EAST start!")
-    for epoch in range(epoch_iter):
+    for epoch in range(start_epoch, epoch_iter):
         model.train()
         epoch_loss = 0
         epoch_time = time.time()
@@ -59,7 +65,7 @@ def train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers,
                         .format(epoch + 1, epoch_iter, i + 1, int(file_num / batch_size), time.time() - start_time,
                                 loss.item()))
 
-        scheduler.step(epoch)  # `optimizer.step()` before `lr_scheduler.step()`
+        scheduler.step()  # `optimizer.step()` before `lr_scheduler.step()`
 
         logger.info('epoch_loss is {:.8f}, epoch_time is {:.8f}'.format(epoch_loss / int(file_num / batch_size),
                                                                   time.time() - epoch_time))
@@ -67,7 +73,9 @@ def train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers,
         # print('=' * 50)
         if (epoch + 1) % interval == 0:
             state_dict = model.module.state_dict() if data_parallel else model.state_dict()
-            torch.save(state_dict, os.path.join(pths_path, 'model_epoch_{}.pth'.format(epoch + 1)))
+            save_pth_name = os.path.join(pths_path, 'model_epoch_{}.pth'.format(epoch + 1))
+            torch.save(state_dict, save_pth_name)
+            logger.info("Epoch model saved to {}".format(save_pth_name))
 
 
 if __name__ == '__main__':
@@ -78,7 +86,10 @@ if __name__ == '__main__':
         os.makedirs(pths_path)
     batch_size = 8
     lr = 1e-3
-    num_workers = 8
+    num_workers = 4
     epoch_iter = 600
     save_interval = 5
-    train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers, epoch_iter, save_interval)
+    start_epoch = 0
+    start_epoch_pth_path = None
+    train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers, epoch_iter, save_interval,
+          start_epoch, start_epoch_pth_path)
