84bc566710e8ffd932f02eb2516a5c416570b870
sgallon <shcmsgallon@outlook.com>
2021-06-22 15:59:13 +0800

    add predict CRNN

--- project3/README.md
+++ project3/README.md
@@ -9,15 +9,19 @@ and then
 
 - recognize the text information in the detected/segmented area.
 
+The training set has 7932 images, and the test set has 1990 images.
+
 ## EAST
 
 EAST is used for detection task.
 
+Codes refer to [SakuraRiven/EAST](https://github.com/SakuraRiven/EAST).
+
 ### Training
 
-Trained for 600 epochs, took roughly 10 days on a 2080Ti GPU.
+Run `$ python ./code/train_EAST.py`.
 
-Code in `train_EAST.py`.
+Trained for 600 epochs, took roughly 10 days on a 2080Ti GPU.
 
 ```
 [2021-06-07 14:34:30,512] [INFO] Epoch is [1/600], mini-batch is [1/991], time consumption is 0.43097448, batch_loss is 9.25167656
@@ -29,11 +33,47 @@ Code in `train_EAST.py`.
 [2021-06-18 00:11:07,143] [INFO] Epoch model saved to /home/jupyter/sjl/neural-network-deep-learning/project3/pths/EAST/model_epoch_600.pth
 ```
 
+### Detection
+
+Detect the text position in the test set and save in `./data/EAST_gt/`.
+
+Run `$ python ./code/eval_EAST.py`. 
+
 ## CRNN
 
 CRNN is used for recognition task.
 
-### Extract All Characters
+Codes refer to [Sierkinhane/CRNN_Chinese_Characters_Rec](https://github.com/Sierkinhane/CRNN_Chinese_Characters_Rec).
+
+### Extract All Characters in Training set
 
 run `$ python ./code/extract_chars.py` to extract all characters in the training set.
 The result is saved as `./code/vocab.txt` in utf-8 encoding.
+
+### Make OCR Dataset
+
+Prepare the dataset for recognition task. 
+Cut the training picture according to labeled text positions, 
+and save each picture as several labeled mini-pictures in `./data/train/text_img/`. 
+(72838 mini-pictures in total.)
+The labels are saved in a single txt as `./data/train/txt/train_own.txt`
+
+### Training
+
+Run `$ python ./code/train_CRNN.py`
+
+Trained for 100 epochs (due to time limitation), took one day.
+
+```
+...
+[2021-06-22 02:57:04,473] [INFO] Start training epoch [99/100]
+...
+[#correct:39175 / #total:72834]
+Test loss: 1.0573, accuray: 0.5379
+[2021-06-22 03:07:10,902] [INFO] Epoch 100, train loss: 0.5379
+[2021-06-22 03:07:10,949] [INFO] Epoch state dict saved to /home/jupyter/sjl/neural-network-deep-learning/project3/pths/CRNN/OWN/crnn/2021-06-21-10-09/checkpoints/checkpoint_99_acc_0.5379.pth
+```
+
+### Generate Result to Submit
+
+Run `$ python .py`
---
+++ project3/code/predict_CRNN.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time        : 2021/6/22 10:14
+# @Author      : sgallon
+# @Email       : shcmsgallon@outlook.com
+# @File        : predict_CRNN.py
+# @Description : predict label on test set using CRNN, use position labels from EAST
+
+import numpy as np
+import time
+import cv2
+import torch
+from torch.autograd import Variable
+import CRNN.model as crnn
+import CRNN.utils as utils
+from CRNN.dataset import get_dataset
+import CRNN.function as function
+import CRNN.alphabets as alphabets
+import yaml
+from easydict import EasyDict as edict
+import argparse
+import sys
+import os
+import shutil
+from PIL import Image
+from torchvision import transforms
+from config import HOME, DATA_ROOT
+from logger_utils import logger
+
+
+def parse_arg():
+    parser = argparse.ArgumentParser(description="CRNN predict")
+    parser.add_argument('--cfg', default=os.path.join(HOME, "code", "CRNN", "CRNN_config.yaml"),
+                        help='experiment configuration filename', type=str)
+    parser.add_argument('-ckpt', '--checkpoint', type=str,
+                        default="/home/jupyter/sjl/neural-network-deep-learning/project3/pths/CRNN/OWN/crnn/2021-06-21-10-09/checkpoints/checkpoint_99_acc_0.5379.pth")
+    parser.add_argument('--mode', default="test", type=str)
+    args = parser.parse_args()
+
+    with open(args.cfg, 'r') as f:
+        # config = yaml.load(f)
+        config = yaml.load(f, Loader=yaml.FullLoader)
+        config = edict(config)
+
+    config.DATASET.ALPHABETS = alphabets.alphabet
+    config.MODEL.NUM_CLASSES = len(config.DATASET.ALPHABETS)
+
+    return config, args
+
+
+def recognition(config, img_path, model, converter, device):
+    inp_h = config.MODEL.IMAGE_SIZE.H
+    max_w = config.MODEL.IMAGE_SIZE.MAX_W
+    mean = np.array(config.DATASET.MEAN, dtype=np.float32)
+    std = np.array(config.DATASET.STD, dtype=np.float32)
+    transformer = transforms.Compose([
+        transforms.ColorJitter(0.5, 0.5, 0.5, 0.25),
+        transforms.ToTensor(),
+    ])
+    normalization = transforms.Compose([
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    img = Image.open(img_path, "r")
+    img = img.convert("RGB")
+
+    # ratio resize
+    img_h, img_w = img.size
+    resize_ratio = inp_h / float(img_h)
+    after_w = int(resize_ratio * img_w)
+    if after_w == 0: after_w = 1
+    img = img.resize((after_w, inp_h), resample=Image.BICUBIC)
+    # w_cur = int(img.shape[1] / (config.MODEL.IMAGE_SIZE.OW / config.MODEL.IMAGE_SIZE.W))
+    # h, w = img.shape
+    # img = cv2.resize(img, (0, 0), fx=w_cur / w, fy=config.MODEL.IMAGE_SIZE.H / h, interpolation=cv2.INTER_CUBIC)
+    # img = np.reshape(img, (config.MODEL.IMAGE_SIZE.H, w_cur, 1))
+
+    # normalize
+    img = transforms.Pad(padding=(0, 0, max_w - after_w, 0), fill=1)(img)  # padding
+    # convert to CNN shape
+    img = transformer(img)  # C, H, W
+
+    # deal with images with channels < 3
+    c, h, w = img.shape
+    if c < 3:
+        n = 3 - c
+        img_new = img
+        for i in range(n):
+            img_new = torch.cat((img_new, img), 0)
+        img = img_new
+    assert img.shape[0] == 3
+
+    img = normalization(img)
+
+    # img = img.astype(np.float32)
+    # img = (img / 255. - config.DATASET.MEAN) / config.DATASET.STD
+    # img = img.transpose([2, 0, 1])
+
+    img = torch.from_numpy(img)
+    img = img.to(device)
+    img = img.view(1, *img.size())
+    model.eval()
+    preds = model(img)
+
+    _, preds = preds.max(2)
+    preds = preds.transpose(1, 0).contiguous().view(-1)
+
+    preds_size = Variable(torch.IntTensor([preds.size(0)]))
+    sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
+    # print('results: {0}'.format(sim_pred))
+
+    return sim_pred
+
+
+if __name__ == '__main__':
+    config, args = parse_arg()
+    logger.info("Config loaded")
+
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+    logger.info("Device is {}".format(device))
+
+    model = crnn.get_crnn(config).to(device)
+    model_state_file = args.checkpoint
+    if model_state_file == '':
+        logger.error(" => no checkpoint found")
+        sys.exit(-1)
+    checkpoint = torch.load(model_state_file, map_location='cpu')
+    model.load_state_dict(checkpoint['state_dict'])
+    logger.info("Model created, checkpoint loaded")
+
+    # converter
+    converter = utils.strLabelConverter(config.DATASET.ALPHABETS)  # get corpus
+    logger.info("Alphabet loaded")
+
+    # get test data
+    input_img_path = os.path.join(DATA_ROOT, "test", "text_img")  # extracted text imgs to load
+    logger.info("Input img path is {}".format(input_img_path))
+    output_txt_path = os.path.join(DATA_ROOT, "test", "txt")  # gt to save
+    logger.info("Output txt path is {}".format(output_txt_path))
+    output_text_file = os.path.join(output_txt_path, 'CRNN_predict_labels.txt')
+    logger.info("Output text file is {}".format(output_text_file))
+    with open(output_text_file, 'w', encoding='utf-8') as f:
+        f.write('')  # 将标注文件其中的内容清空
+    logger.info("Output text file cleared")
+
+    imgs = os.listdir(input_img_path)
+    for idx, filename in enumerate(imgs):
+        if not filename.endswith(".jpg"):
+            logger.info("[{}/{}]Skip non-jpg file: {}".format(idx, len(imgs), filename))
+            continue
+        logger.info("[{}/{}]Processing picture: {}".format(idx, len(imgs), filename))
+
+        img_path = os.path.join(input_img_path, filename)
+        pred = recognition(config, img_path, model, converter, device)
+
+        with open(output_text_file, 'a', encoding='utf-8') as f:
+            f.write(filename + ',' + pred + '\n')
+
+    logger.info("All done")
